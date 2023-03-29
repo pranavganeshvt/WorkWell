bafbaf9fcb9a01a88c249c2abe92a60298e8d6e6
yuvavt <122953122+yuvavt@users.noreply.github.com>
2023-03-29 00:57:23 -0400

    Navigation Bar page

---
+++ script.js
@@ -0,0 +1,7 @@
+// const navLinkEls = document.querySelectorAll('.nav-links');
+// const windowPathname = window.location.pathname;
+// navLinkEls.forEach(navLinkEL=>{
+//     if(navLinkEL.href.includes(windowPathname)){
+//         navLinkEL.classList.add('active');
+//     }
+// });
