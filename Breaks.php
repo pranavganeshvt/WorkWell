adab23546632113d9d4f7201e38026df5d9abae3
SindhujaReddy-VT <bsindhuja@vt.edu>
2023-05-05 03:16:23 -0400

    Sindhu's final changes

--- Breaks.php
+++ Breaks.php
@@ -87,7 +87,7 @@
                             <img src="images/editprofile1.png">
                             <p>Edit Profile</p>
                             <span>></span>
-                            <a href="#" class="sub-menu-link">
+                            <a href="help.html" class="sub-menu-link">
                                 <img src="images/help.png">
                                 <p>Help</p>
                                 <span>></span>
